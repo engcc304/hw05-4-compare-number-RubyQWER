9eeadabcdcc1bcda42b2b975c7f60ced0498fa5a
RubyQWER <Chanatpong1234@gmail.com>
2023-08-17 01:36:37 +0700

    hw05-4

--- Homework/HW05/hw05-4-compare-number.cpp
+++ Homework/HW05/hw05-4-compare-number.cpp
@@ -25,3 +25,26 @@
     Output:
         Number1 equal Number2
 */
+#include <stdio.h>
+int main() {
+
+    int a = 0, b = 0 ;
+    printf("Input[1] : ") ;
+    scanf("%d", &a);
+    printf("Input[2] : ") ;
+    scanf("%d", &b);
+
+    if (a > b) {
+        printf("Number1 greater than Number2") ;
+
+    }   else if (b > a) {
+            printf("Number1 lower than Number2") ;
+
+        }   else if (a == b) {
+                printf("Number1 equal Number2") ;
+            }
+            else printf("Error") ;
+    
+    
+    return 0 ;
+}// end function
